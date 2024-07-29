d8fbd423b3f258e2999443d68cace01c60d6c7e4
sonu <sonu0829>
2024-07-29 16:33:05 +0530

    second commit

--- src/main/java/com/javaBootLearn/controller/HelloWorldController.java
+++ src/main/java/com/javaBootLearn/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.javatpoint.controller;
+package com.javaBootLearn.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
